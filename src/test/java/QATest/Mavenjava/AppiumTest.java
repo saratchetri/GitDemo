46741457862fa025e1d24a1642099b9a62f293b9
Sarat <sarat.chetri@gmail.com>
2020-08-27 11:05:12 +0530

    first commit

---
+++ src/test/java/QATest/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package QATest.Mavenjava;
+
+import org.junit.Test;
+
+public class AppiumTest {
+
+	
+
+	@Test
+	public void NativeAppAndriod() {
+		System.out.println("NativeAppAndriod");
+		
+	}
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/QATest/Mavenjava/SeleniumTest.java
@@ -0,0 +1,16 @@
+package QATest.Mavenjava;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+}
